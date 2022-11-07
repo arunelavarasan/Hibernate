eb1b6c712292e8401767ade3134c3ccc52322437
arunelavarasan <88072148+arunelavarasan@users.noreply.github.com>
2022-11-07 17:06:32 +0530

    Add files via upload

---
+++ Student/src/main/java/org/jsp/Student.java
@@ -0,0 +1,56 @@
+package org.jsp;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Id;
+import javax.persistence.Persistence;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Student {
+	@Id
+	private int id;
+	private String name;
+	private int age;
+
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("dev");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		Student student = new Student();
+		student.setId(1);
+		student.setName("Arun");
+		student.setAge(22);
+		entityTransaction.begin();
+		entityManager.persist(student);
+		entityTransaction.commit();
+	}
+}
